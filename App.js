59d16e268943d2209d6e9e4a38448dd0e594b300
harrdy25 <kukadiya.hardik1@gmail.com>
2022-04-14 17:22:44 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+
+import { View, Text } from 'react-native'
+import React from 'react'
+import Demo from './src/screen/Demo'
+
+export default function App() {
+  return (
+    <View>
+      <Demo />
+    </View>
+  )
+}
