546c4d0227d5241028bb6acbf0a03feb554d2582
harrdy25 <kukadiya.hardik1@gmail.com>
2022-04-28 16:14:07 +0530

    28_04

---
+++ src/screen/KBC/KbcHomePage.js
@@ -0,0 +1,126 @@
+import { View, Text, SafeAreaView, Image, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { images } from '../../assets/images'
+import { normalize } from '../../utils'
+import colors from '../../theme/colors'
+
+function KbcHomePage() {
+
+    const [select, setSelect] = useState(0);
+
+    return (
+        <SafeAreaView style={{ flex: 1}}>
+            <View style={{ flex: 1,}}>
+                <View style={styles.Card}>
+                    <Image style={styles.Logo} source={images.IMG_KBC_LOGO} />
+                    <Text style={styles.TitleName}>Kaun Banega Crorepati</Text>
+                </View>
+                <View style={styles.Box}>
+                    <Text style={styles.SelectLanguage}>Choose Language</Text>
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                    <TouchableOpacity style={select === 1 ? styles.LanguageBoxGreen : styles.LanguageBox} onPress={() => setSelect(1)}>
+                        <Text style={styles.SelectLanguage}>English</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={select === 2 ? styles.LanguageBoxGreen : styles.LanguageBox} onPress={() => setSelect(2)}>
+                        <Text style={styles.SelectLanguage}>Hindi</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={select === 3 ? styles.LanguageBoxGreen : styles.LanguageBox} onPress={() => setSelect(3)}>
+                        <Text style={styles.SelectLanguage}>Gujrati</Text>
+                    </TouchableOpacity>
+                </View>
+                <Image style={styles.KBCLogo} source={images.IMG_KBC_LOGO} />
+                <TouchableOpacity style={styles.Box}>
+                    <Text style={styles.StartQuiz}>Start Quiz</Text>
+                </TouchableOpacity>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginTop: normalize(20) }}>
+                    <Image style={styles.ImageIcon} source={images.IMG_Sound_PNG} />
+                    <Image style={styles.ImageIcon} source={images.IMG_Star_PNG} />
+                    <Image style={styles.ImageIcon} source={images.IMG_Share_PNG} />
+                    <Image style={styles.ImageIcon} source={images.IMG_Ellips_PNG} />
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    BGImage: {
+        flex: 1
+    },
+    Card: {
+        margin: normalize(5),
+        borderRadius: normalize(15),
+        backgroundColor: '#38006b',
+        shadowColor: colors.black,
+        shadowOffset: {
+            width: 0.5,
+            height: 0.5,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 10.0,
+        elevation: 5,
+        zIndex: 1,
+        flexDirection: 'row',
+    },
+    TitleName: {
+        fontSize: normalize(25),
+        fontWeight: 'bold',
+        color: colors.extraLight,
+        alignSelf: 'center',
+        textAlign: 'center',
+        flex: 1,
+        marginRight: normalize(25)
+    },
+    Logo: {
+        height: normalize(50),
+        width: normalize(50),
+        borderRadius: normalize(30),
+    },
+    KBCLogo: {
+        borderRadius: normalize(200),
+        alignSelf: 'center',
+        marginVertical: '8%'
+    },
+    Box: {
+        borderRadius: normalize(8),
+        alignSelf: 'center',
+        marginTop: normalize(10),
+        backgroundColor: '#6a1b9a'
+    },
+    SelectLanguage: {
+        fontSize: normalize(18),
+        fontWeight: '700',
+        color: colors.white,
+        alignSelf: 'center',
+        padding: normalize(6)
+    },
+    LanguageBox: {
+        borderRadius: normalize(8),
+        alignSelf: 'center',
+        marginTop: normalize(10),
+        backgroundColor: '#9c4dcc',
+    },
+    LanguageBoxGreen: {
+        borderRadius: normalize(8),
+        alignSelf: 'center',
+        marginTop: normalize(10),
+        backgroundColor: 'green',
+        // borderTopEndRadius: normalize(20),
+        // borderBottomLeftRadius: normalize(20)
+    },
+    StartQuiz: {
+        fontSize: normalize(22),
+        fontWeight: '700',
+        color: colors.white,
+        alignSelf: 'center',
+        padding: normalize(8)
+    },
+    ImageIcon: {
+        height: normalize(35),
+        width: normalize(35),
+        tintColor: '#9c4dcc'
+    }
+});
+
+export default KbcHomePage;
---
+++ src/screen/KBC/KbcResult.js
@@ -0,0 +1,172 @@
+import { View, Text, SafeAreaView, Image, StyleSheet, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { images } from '../../assets/images';
+import colors from '../../theme/colors';
+import { normalize } from '../../utils';
+
+function KbcResult() {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ flex: 1, }}>
+                <View style={styles.Card}>
+                    <Image style={styles.Logo} source={images.IMG_KBC_LOGO} />
+                    <Text style={styles.TitleName}>KBC Result</Text>
+                </View>
+                <View style={{ margin: normalize(10) }}>                   
+                    <Image style={styles.Trophy} source={images.IMG_TROPHY} />
+                    <View style={styles.CardCongret}>
+                        <Text style={styles.Result}>🎊 🎉 Congratulations 🎊 🎉</Text>
+                    </View>
+                </View>
+                <View style={styles.CardResultBoX}>
+                    <Text style={styles.Result}>-:: Your Score ::-</Text>
+                    <Text style={styles.ResultScore}>-:: 12 ::-</Text>
+                </View>
+                <View style={styles.CardResultBoX}>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: normalize(25) }}>
+                        <View style={{ flexDirection: 'row' }}>
+                            <Text style={styles.Result}>Correct</Text>
+                            <Image style={styles.TrueIcon} source={images.IMG_True_PNG} />
+                        </View>
+                        <View style={{ flexDirection: 'row' }}>
+                            <Text style={styles.Result}>InCorrect</Text>
+                            <Image style={styles.WrongIcon} source={images.IMG_Wrong_PNG} />
+                        </View>
+                    </View>
+                    <View style={styles.ScoreBox}>
+                        <Text style={styles.ResultScore}>12</Text>
+                        <Text style={styles.ResultScore}>03</Text>
+                    </View>
+                </View>
+                <TouchableOpacity style={styles.Box}>
+                    <Text style={styles.PlayAgain}>Play Again!!</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    Card: {
+        margin: normalize(5),
+        borderRadius: normalize(15),
+        backgroundColor: '#38006b',
+        shadowColor: colors.black,
+        shadowOffset: {
+            width: 0.5,
+            height: 0.5,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 10.0,
+        elevation: 5,
+        zIndex: 1,
+        flexDirection: 'row',
+    },
+    TitleName: {
+        fontSize: normalize(25),
+        fontWeight: 'bold',
+        color: colors.extraLight,
+        alignSelf: 'center',
+        textAlign: 'center',
+        flex: 1,
+        marginRight: normalize(25)
+    },
+    Logo: {
+        height: normalize(50),
+        width: normalize(50),
+        borderRadius: normalize(30),
+    },
+    Result: {
+        fontSize: normalize(25),
+        fontWeight: '600',
+        alignSelf: 'center',
+        color: '#FFFFFF',
+        padding: normalize(5)
+    },
+    Trophy: {
+        height: normalize(150),
+        width: normalize(150),
+        alignSelf: 'center',
+        margin: normalize(10)
+    },
+    CardCongret: {
+        margin: normalize(10),
+        borderRadius: normalize(15),
+        backgroundColor: '#38006b',
+        shadowColor: colors.black,
+        shadowOffset: {
+            width: 0.5,
+            height: 0.5,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 10.0,
+        elevation: 5,
+        zIndex: 1,
+    },
+    ResultScore: {
+        fontSize: normalize(50),
+        fontWeight: '900',
+        alignSelf: 'center',
+        color: '#FFFFFF',
+    },
+    CardResultBoX: {
+        margin: normalize(10),
+        borderRadius: normalize(15),
+        backgroundColor: '#9c4dcc',
+        shadowColor: colors.black,
+        shadowOffset: {
+            width: 0.5,
+            height: 0.5,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 10.0,
+        elevation: 5,
+        zIndex: 1,
+    },
+    TrueIcon: {
+        height: normalize(50),
+        width: normalize(50),
+        tintColor: 'green'
+    },
+    WrongIcon: {
+        height: normalize(50),
+        width: normalize(50),
+        tintColor: 'red'
+    },
+    Box: {
+        borderColor: colors.white,
+        borderRadius: normalize(10),
+        backgroundColor: '#38006b',
+        margin: normalize(10),
+        marginTop: normalize(50)
+    },
+    PlayAgain: {
+        fontSize: normalize(25),
+        fontWeight: '600',
+        alignSelf: 'center',
+        color: '#FFFFFF',
+        padding: normalize(8)
+    },
+    ScoreBox: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        margin: normalize(10),
+        borderRadius: normalize(15),
+        backgroundColor: '#6a1b9a',
+        shadowColor: colors.black,
+        shadowOffset: {
+            width: 0.5,
+            height: 0.5,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 10.0,
+        elevation: 5,
+        zIndex: 1,
+    }
+});
+
+export default KbcResult;
+
+// #38006b
+// #6a1b9a
+// #9c4dcc
